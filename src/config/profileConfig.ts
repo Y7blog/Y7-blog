767266ba423656ee93b45bdaeb897faaf4bc6d16
Y7blog <btc0x100u@gmail.com>
2026-09-08 04:18:42 +0800

    图片和文字

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "https://www.y7img.ccwu.cc/file/头像/1788789438317_头像1_4K高清.png",
 
 	// 名字
-	name: "Firefly",
+	name: "鱼七",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "认真赚钱，好好生活！",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -22,15 +22,15 @@ export const profileConfig: ProfileConfig = {
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
-			name: "qq",
+			name: "TG",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://t.me/+ZegDSfLwOjFiOTJl",
 			showName: false,
 		},
 		{
-			name: "GitHub",
+			name: "X",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://x.com/0x100U",
 			showName: false,
 		},
 		{
