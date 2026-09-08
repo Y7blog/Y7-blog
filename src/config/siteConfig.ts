e2fb7a7c203bceff457277ee2b2eff551f5996a6
Y7blog <btc0x100u@gmail.com>
2026-09-08 17:08:48 +0800

    浏览器标

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -92,7 +92,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			 src: "/assets/images/logo/favicon-gold.svg",
+			 src: "public/favicon/fish-32.png",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
