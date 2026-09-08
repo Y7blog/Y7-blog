95c7a94d737d944c42156ca4612a35aada53198e
Y7blog <btc0x100u@gmail.com>
2026-09-08 16:28:39 +0800

    网站标签LOGO

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -92,7 +92,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/fish-gold32.png",
+			 src: "/assets/images/logo/favicon-gold.svg",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
