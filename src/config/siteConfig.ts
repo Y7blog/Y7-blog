213421eb24c2fce9fb3d90ff6c30cb4d9d9fa48e
Y7blog <btc0x100u@gmail.com>
2026-09-08 12:13:05 +0800

    图标

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -92,7 +92,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/firefly-32.png",
+			src: "/favicon/fish-gold32.png",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
@@ -113,9 +113,9 @@ export const siteConfig: SiteConfig = {
 		// 使用 Astro 图标库时不需要设置 valueDark，图标会自动跟随主题亮暗色切换
 		logo: {
 			type: "image",
-			value: "assets/images/logo/firefly-light.png",
-			valueDark: "assets/images/logo/firefly-dark.png",
-			alt: "🍀",
+			value: "assets/images/logo/fish-light.png",
+			valueDark: "assets/images/logo/fish-dark.png",
+			alt: "Y7",
 		},
 		// 导航栏标题
 		title: "鱼七的金库",
